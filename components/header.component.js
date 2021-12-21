a8c4fbc4790bb9fbb6b21d344903cf382a57aa51
Tyler Roth <tyler.roth09@gmail.com>
2021-12-21 09:26:43 -0500

    remove linter warning

--- components/header.component.js
+++ components/header.component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ImageBackground, Text, StyleSheet} from 'react-native';
+import {ImageBackground, StyleSheet} from 'react-native';
 
 const Header = () => {
   return (
